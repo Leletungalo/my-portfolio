dc45aae6edc70d4284db3630151e24603b9b1336
leletu ngalo <lelethungalo@gmail.com>
2020-04-15 01:38:11 +0200

    first commit 2

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname,"public")));
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`running at ${PORT}`));
