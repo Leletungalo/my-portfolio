81600bc3449458cbfb17bcd55ba79d592bcbca28
leletu ngalo <lelethungalo@gmail.com>
2020-10-12 03:55:14 +0200

    removed express

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-app.use(express.static(path.join(__dirname,"public")));
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => console.log(`running at ${PORT}`));
