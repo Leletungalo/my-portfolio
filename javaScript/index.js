c4f5385a3fdfefe25be1921e897a152b9699ca52
leletu ngalo <lelethungalo@gmail.com>
2020-10-22 21:12:50 +0200

    before add jobs

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,7 +2,7 @@ const projectsImage = document.querySelectorAll(".projects-image");
 const projects = document.querySelector("#projects");
 projectsImage.forEach(image => image.addEventListener("click",() =>{
   const model = `
-  <div class="model-back">
+  <div class="model-back" name="backDrop">
     <div class="model">
       <img src="${image.getAttribute("src")}" class="model-images">
       <div class="left">
@@ -16,18 +16,25 @@ projectsImage.forEach(image => image.addEventListener("click",() =>{
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quam? Odio suscipit obcaecati quas quo.
         </p>
-        <div >
-          <a href="https://github.com/Leletungalo"><img class="model-socia" src="./images/git-hub.png" alt=""></a>
-          <a href=""><img class="model-socia" src="./images/git-hub.png" alt=""></a>
+        <div class="model-social">
+          <a href="https://github.com/Leletungalo" target="_blank" >
+            <img class="model-socia-left" src="./images/git-hub.png" alt="">
+          </a>
+          <a >
+          <img class="model-socia-right" src="./images/preview.svg" alt="">
+          </a>
         </div>
       </div>
     </div>
   </div>`
 projects.insertAdjacentHTML("afterbegin",model);
   const back = document.querySelector(".model-back");
-  back.addEventListener("click", closeBack.bind(this,back));
+  back.addEventListener("click", closeBack);
 }))
 
-const closeBack = (back) => {
-  back.style.display = "none"
+const closeBack = (event) => {
+  if(event.target.getAttribute("name") === "backDrop"){
+    const back = document.querySelector(".model-back");
+    back.style.display = "none"
+  }
 }
